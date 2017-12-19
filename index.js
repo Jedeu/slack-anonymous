8d86e88746de5166e2b4ab8f2e08da737774410d
Jed <Jedeu@users.noreply.github.com>
2017-12-18 19:05:52 -0800

    Update index.js
    Re-added logs for debugging

--- index.js
+++ index.js
@@ -77,6 +77,8 @@ function createResponsePayload(requestBody) {
     var remainingText = splitted.slice(1).join(' ');
     remainingText = 'Someone said "' + remainingText + '"';
     
+    console.log("Returning the following payload: ", target, hookUrls[target]);
+    
     return {
         target: target,
         text: remainingText,
